16cc85fb069a80c605ff4b0098ccc137c01d7329
iancarpenter <ianhcarpenter@gmail.com>
2023-01-08 21:26:15 +0000

    Add project files.

---
+++ PigPdfPoC/Program.cs
@@ -0,0 +1,24 @@
+﻿using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+namespace PigPdfPoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (PdfDocument document = PdfDocument.Open(@"C:\Temp\EMegane.pdf"))
+            {
+                foreach (Page page in document.GetPages())
+                {
+                    string pageText = page.Text; 
+                    
+                    foreach (Word word in page.GetWords())
+                    {
+                        Console.WriteLine(word.Text);
+                    }
+                }
+            }
+        }
+    }
+}
